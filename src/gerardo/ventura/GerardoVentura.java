83c389ea3cd2dab1e4f3f432d2261689e42ffff8
T- <T-@PC243>
2017-05-18 19:36:22 -0500

    Clase del jueves 18 mayo

--- src/gerardo/ventura/GerardoVentura.java
+++ src/gerardo/ventura/GerardoVentura.java
@@ -16,6 +16,16 @@ public class GerardoVentura {
      */
     public static void main(String[] args) {
         System.out.println("HOLA MUNDO CRUEL");
+        //Probaremos nuestra solucion
+        //Crear un usuario hipotetico
+        Usuario u=new Usuario();
+        //Le asignamos valores
+        u.altura=1.68f;
+        u.peso=73f;
+        //Generamos el modelo
+        Imc imc=new Imc();
+       
+        System.out.println("El valor del imc es"+ imc.calcular(u));
     }
     
 }
---
+++ src/gerardo/ventura/Imc.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gerardo.ventura;
+
+/**
+ *
+ * @author T-
+ */
+public class Imc {
+    public float calcular(Usuario u){
+        //Aqui va la logica del calculo
+        float resultado= u.peso/(u.altura*u.altura);
+        return resultado;
+    }
+    
+}
